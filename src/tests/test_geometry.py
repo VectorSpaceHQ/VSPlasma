8e784f96119fe5c23361a9ce1c35b0ef0eff8ed1
Adam Spontarelli <adam@vector-space.org>
2022-02-01 15:31:51 -0500

    init commit at pytest. not functioning yet

---
+++ src/tests/test_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Unit tests against the geometry.py classes
+import sys
+import pytest
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import Qt
+import vsplasma as vsp
+
+
+def test_basic(qtbot):
+    app = QApplication(sys.argv)
+    window = vsp.MainWindow(app)
+
+    qtbot.addWidget(window)
+
+    qtbot.mouseClick(window.actionImport, QtCore.Qt.LeftButton)
+
+    print(window.geometry.parts)
+
+    assert 0
